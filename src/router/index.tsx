49b4c1e77a9f1d5acca06d7cd211eb89704202c2
gqian <2748602682@qq.com>
2023-06-21 19:24:08 +0800

    "路由懒加载"

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,19 @@
-
+import React,{ lazy } from 'react'
 import Home from '../views/Home'
-import About from '../views/about'
+// import About from '../views/about'
 import {Navigate} from 'react-router-dom'
+
+const About = lazy(()=>import('../views/about'))
+
+// 报错：A component suspended while responding to synchronous input. This will cause the UI to be replaced with a loading indicator. To fix, updates that suspend should be wrapped with startTransition.
+// 路由懒加载的组件，外层必须套一个loading的组件
+
+
+const withLoadingComponents = (comp:JSX.Element) =>(
+    <React.Suspense fallback={<div>loading...</div>}>
+            {comp}
+        </React.Suspense>
+)
 const routes = [
     {
         path:"/",
@@ -13,7 +25,7 @@ const routes = [
     },
     {
         path:"/about",
-        element:<About/>
+        element:withLoadingComponents(<About/>) 
     },
 ]
 
